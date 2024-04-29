b0bb7c1b28f771bf21248b6ea5630a722c66f0f1
xterkanter <xterkanter@gmail.com>
2024-04-30 01:54:30 +0300

    feat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import {Navbar} from "@/components/navbar";
 import {Hero} from "@/components/sections/Hero";
-import {ShivaMarquee} from "@/components/shiva-marquee";
+import {ShivaMarquee, ShivaSingleMarquee} from "@/components/shiva-marquee";
 import {SocialLinksSection} from "@/components/sections/socials";
 import {LinksSection} from "@/components/sections/links";
 import {TokenAdress} from "@/components/sections/token-adress";
@@ -9,6 +9,7 @@ export default function Home() {
 
     return (
         <main className="max-w-[100vw] overflow-x-hidden">
+            {/*<ShivaSingleMarquee className="absolute -mt-[40px] left-0 right-0" />*/}
             <div className="relative w-full min-h-screen flex flex-col items-center bg-black z-20">
                 <div className="flex relative w-full max-w-[1200px] m-auto flex-col items-center">
                     <Navbar/>
--- src/components/shiva-marquee.tsx
+++ src/components/shiva-marquee.tsx
@@ -1,25 +1,38 @@
 'use client'
 import {motion} from "framer-motion";
 import Marquee from "react-fast-marquee";
+import cx from "clsx";
 
-const getShivaMarqueeContent = () => new Array(20).fill('SHIVA INU').map((text, index) => <span key={index} className="mx-1">{text}</span>)
+export const getShivaMarqueeContent = () => new Array(20).fill('SHIVA INU').map((text, index) => <span key={index} className="mx-1">{text}</span>)
+
+export const ShivaSingleMarquee = ({ className }: { className?: string}) => {
+    return (
+        <div
+            className={cx("z-1 w-screen h-12 bg-white font-bold flex items-center justify-center text-black whitespace-nowrap", className)}
+        >
+            <Marquee>
+                {getShivaMarqueeContent()}
+            </Marquee>
+        </div>
+    )
+}
 
 export const ShivaMarquee = () => (
     <div className="h-10">
-        <motion.div
+        <div
             className="w-screen rotate-1 h-12 bg-neutral-400 font-bold flex items-center justify-center text-black whitespace-nowrap"
         >
             <Marquee direction="right">
                 {getShivaMarqueeContent()}
             </Marquee>
-        </motion.div>
-        <motion.div
+        </div>
+        <div
             className="w-screen -rotate-2 -translate-y-10 h-12 bg-white font-bold flex items-center justify-center text-black whitespace-nowrap"
         >
             <Marquee>
                 {getShivaMarqueeContent()}
             </Marquee>
-        </motion.div>
+        </div>
     </div>
 
 )
